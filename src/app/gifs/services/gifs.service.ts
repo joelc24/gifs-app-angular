bfa2a217f89334d013d2183b3fbcd6d134830a07
Joel cues <libra-marriaga@hotmail.com>
2023-02-16 18:12:22 -0500

    apikey obtenida

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  private apiKey: string = '8sg4AkVXVyBkb8fs6AtIUnEWIkYwjz9G'
+  
+  private _historial: string[] = []
+
+  get historial(){
+    return [...this._historial]
+  }
+
+  buscarGifs( query: string = '' ){
+
+    query = query.trim().toLowerCase()
+
+    if ( !this._historial.includes( query ) ) {
+      this._historial.unshift( query )
+      this._historial = this._historial.splice(0,10)
+    }
+    console.log(this._historial)
+  }
+
+  constructor() { }
+}
